2b2ad3dce4f1a77f51a12ba1182526af22c50084
Samuel <knipknap@gmail.com>
2020-06-27 21:09:55 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #/bin/sh
 
-pip3 install -r $INPUT_REQUIREMENTS
+pip3 install -r "$INPUT_REQUIREMENTS"
 
-cd $INPUT_MANAGE_PY_DIR
+cd "$INPUT_MANAGE_PY_DIR"
 python manage.py collectstatic --noinput
 
 cd -
-minify -o $INPUT_STATIC_OUT_DIR $INPUT_IN_STATIC_DIR
+minify -o "$INPUT_STATIC_OUT_DIR" "$INPUT_IN_STATIC_DIR"
