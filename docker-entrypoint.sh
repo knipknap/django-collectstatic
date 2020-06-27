792c15fa4a1f262b060b99188832426a13e4e1d8
Samuel <knipknap@gmail.com>
2020-06-27 21:21:29 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,8 +6,8 @@ cd "$INPUT_MANAGE_PY_DIR"
 python manage.py collectstatic --noinput
 
 cd "$INPUT_IN_STATIC_DIR"
-mkdir -p "$INPUT_STATIC_OUT_DIR"
 
 find . -name "*.js" -o -name "*.css" -o -name "*.html" | while read infile; do
-  minify -o /github/workspace/"$INPUT_STATIC_OUT_DIR/$infile" "$infile"
+  mkdir -p "$INPUT_STATIC_OUT_DIR/$(basename $infile)"
+  minify -ro "/github/workspace/$INPUT_STATIC_OUT_DIR/$infile" "$infile"
 done
